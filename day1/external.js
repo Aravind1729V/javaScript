261704ecde4a181c54fdd8801ba66155a52191e1
V ARAVIND <aravindaravindachari55@gmail.com>
2024-03-16 19:03:37 +0530

    day1

---
+++ day1/external.js
@@ -0,0 +1,8 @@
+console.log("i am external js")
+let demo = document.getElementById("demo")
+console.log(demo)
+demo.style.backgroundColor = "skyblue"
+demo.style.color = "red"
+// let demo1 = document.getElementsByClassName("demo1")
+// demo1.style.backgroundColor="green"
+// demo1.style.color="blue"
