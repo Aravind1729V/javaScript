8865c70fe26615344799a69b7dc23716ba952965
V ARAVIND <aravindaravindachari55@gmail.com>
2024-03-21 17:44:57 +0530

    day4

---
+++ day4/function.js
@@ -0,0 +1,47 @@
+// function demo()
+// {
+//     console.log("hello world");
+// }
+// console.log(demo);
+// demo()
+// console.log(demo());
+
+// //..........................
+
+
+// console.log(demo);
+// console.log(demo1());
+// console.log(demo1);
+// function demo1()
+// {
+//     return "hello world";
+// }
+// console.log(demo1());
+// console.log(demo1);
+
+//eg:->2
+
+// function add(a ,b) 
+// {
+//     let c = a + b ;
+//     return  c;
+// }
+// console.log(add);
+
+// console.log(add(5,5));
+// console.log(add(5+5,10));
+
+
+// let a = Number(prompt("enter a value"))
+// let b = Number(prompt("enter b value"))
+
+// console.log(a + b);
+
+let m = Number(prompt("enter m value"))
+let n = Number(prompt("enter n value"))
+function add1() {
+    let o = m + n;
+    return o
+}
+console.log(add1());
+
