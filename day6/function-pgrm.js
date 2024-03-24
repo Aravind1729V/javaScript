068aabe9995fd11a6f7e817431b1e28ac761c4b1
V ARAVIND <aravindaravindachari55@gmail.com>
2024-03-24 15:10:52 +0530

    day6

---
+++ day6/function-pgrm.js
@@ -0,0 +1,36 @@
+// // eg 1
+
+// function main(a)
+// {
+//     let c = a();
+//     return c
+// }
+
+// let demo = main(function(){
+//     console.log('hello');
+// })
+
+// // console.log(main);
+
+// // console.log(demo);
+// demo
+
+// let demo1 = main(function(){
+//     console.log("java");
+// })
+// demo1
+
+// function main(a,b)
+// {
+//     let c = a + b;
+//     return c
+// }
+// let demo = main(20,30)
+// console.log(demo);
+
+
+
+// create  a HOF and CBF  to perform arithmetic operation of two numbers using 
+// function declaration statement and function expression 
+// note: input is given by end users
+
