a5bb6ad9c68b914e825be1fbc108f39035301a8d
V ARAVIND <aravindaravindachari55@gmail.com>
2024-03-27 17:47:18 +0530

    day2

---
+++ day2/main.js
@@ -0,0 +1,105 @@
+'use strict'
+
+// // console.log("start")
+
+// //scope
+
+// //global
+
+// // var a = 10;
+// // console.log(a);
+
+// //local
+// // let b = 20;
+// // console.log(b);
+
+// // const c=30;
+// // console.log(c);
+ 
+// // difference between var let and const
+
+// //scope
+
+// var a = 10;
+// console.log(a);
+// console.log(window);
+
+// let b = 20;
+// console.log(b);
+
+// const c = 30;
+// console.log(c);
+
+// console.log(window);
+
+// //declaration
+
+// var a;
+// console.log(a);
+
+// let d;
+// console.log(d);
+
+// // const e;
+// // console.log(e);
+
+// //declaration and initialization
+
+// var a1 = 10;
+// console.log(a1);
+
+// let b1 =20;
+// console.log(b1);
+
+// const c1=30;
+// console.log(c1);
+
+// //Re-inti
+
+// var a2 =10;
+//     a2 =20;
+// console.log(a2);
+
+// let b2 = 20;
+//     b2 = 30;
+// console.log(b2);
+
+// const c2 =30;
+//     c2 = 40;
+// console.log(c2);
+
+// Re-init and Re-dec
+
+// var a3=10;
+// console.log(a3);
+
+// var a3=20;
+// console.log(a3);
+// console.log(a3);
+
+// let b3=30;
+// console.log(b3);
+
+// let b3 = 40;
+// console.log(b3);
+
+// const c3=10;
+// console.log(c3);
+
+// const c3=34;
+// console.log(c3);
+
+// var a = 90;
+// console.log(a);
+// console.log(window)
+
+// hoisting
+
+console.log(a);
+var a= 10;
+
+// console.log(b);
+// let b = 20;
+
+// console.log(c);
+// const c= 30;
